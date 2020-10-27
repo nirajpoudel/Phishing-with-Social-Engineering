1d8fd1078918b6805498a7e676ab06b249b21927
Niraj <59787504+nirajpoudel18@users.noreply.github.com>
2020-10-27 07:41:13 +0545

    Update settings.py

--- FacebookClone/FacebookClone/settings.py
+++ FacebookClone/FacebookClone/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dw%m9nn)=qkth&j8pdwkr%kozojb8&=b!@@!5r0fh-!=q&7$pr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
